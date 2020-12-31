10f9961784481a30273ba21fc5ee183e97b58104
Tommy D. Michailidis <tommy.05.tdm@gmail.com>
2020-12-31 16:15:33 -0500

    api

--- src/api/fetchWether.js
+++ src/api/fetchWether.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
-const API_KEY = process.env.API_KEY;
+const API_KEY = '3b0afc94effac3fedf42ee23c9fcc0b4';
 
 export const fetchWether = async (query) => {
   const { data } = await axios.get(URL, {
